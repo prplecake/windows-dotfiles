eda87e25838db99ba3a6025ca0540a6ec93ded28
Matthew Jorgensen <mjorgensen@fortressmedical.com>
2021-01-08 13:36:20 -0600

    Use LiteralPath instead of Path

--- functions/registry_functions.ps1
+++ functions/registry_functions.ps1
@@ -2,13 +2,13 @@ function Disable-ShakeToMinimize {
     $registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
     $name = "DisallowShaking"
     $value = 1
-    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+    New-ItemProperty -LiteralPath $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
     Write-Host "Shake to Minimize disabled."
 }
 function Enable-ShakeToMinimize {
     $registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
     $name = "DisallowShaking"
     $value = 0
-    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+    New-ItemProperty -LiteralPath $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
     Write-Host "Shake to Minimize enabled."
 }
