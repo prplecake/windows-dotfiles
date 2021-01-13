7fc5a97c71ada4d33d382198ac093b426cf2643f
Matthew Jorgensen <mjorgensen@fortressmedical.com>
2021-01-13 17:36:34 -0600

    Add function and alias to open both Sublime Text and Sublime Merge

--- aliases.ps1
+++ aliases.ps1
@@ -5,5 +5,8 @@
 Set-Alias -Name ll -Value ls
 Set-Alias -Name which -Value Get-Command
 
+function Open-SublimeTextAndMerge { & smerge $args; & subl $args }
+New-Alias -Name lime -Value Open-SublimeTextAndMerge
+
 # Source git aliases
 . "$wd\git-aliases.ps1"
