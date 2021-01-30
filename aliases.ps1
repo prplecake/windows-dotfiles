778c720f011e67d211ff7eb730a203904d9a725e
Matthew Jorgensen <mjorgensen@fortressmedical.com>
2021-01-29 21:49:32 -0600

    Add some customized ping commands

--- aliases.ps1
+++ aliases.ps1
@@ -9,5 +9,15 @@ Set-Alias -Name ydl -Value youtube-dl
 function Open-SublimeTextAndMerge { & smerge $args; & subl $args }
 New-Alias -Name lime -Value Open-SublimeTextAndMerge
 
+function Test-HostReachability {
+    test-connection $args -Repeat | select @{n='TimeStamp';e={Get-Date}}, Ping, Destination, Address, Latency | ft
+}
+Set-Alias -Name ping2 -Value Test-HostReachability
+
+function Better-Ping {
+    & ping.exe $args -t
+}
+Set-Alias -Name ping -Value Better-Ping
+
 # Source git aliases
 . "$wd\git-aliases.ps1"
