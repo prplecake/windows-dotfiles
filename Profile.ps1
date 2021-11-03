9a601722a80361472c7404d006ae0407b91deb44
Matthew Jorgensen <mjorgensen@fortressmedical.com>
2021-11-03 14:04:42 -0500

    Import posh-git and set WindowTitle

--- Profile.ps1
+++ Profile.ps1
@@ -5,3 +5,13 @@ $wd = Split-Path $PROFILE
 
 # Exit PowerShell on Ctrl+D
 Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
+
+# Import Posh-Git
+Import-Module Posh-Git
+
+# Set window title to something more sensible than
+# 'C:\Users\$username - PowerShell $version ($pid)'
+$global:GitPromptSettings.WindowTitle = {
+    param($GitStatus, [bool]$IsAdmin)
+    "PS $($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor)"
+}
