ae3ebc50981cc131df63c224d52f1c90ce5e4f9b
Matthew Jorgensen <matthew.jorgensen@evidentiq.com>
2024-05-07 13:36:18 -0500

    Add Add-FleetContextMenu function

--- functions/registry_functions.ps1
+++ functions/registry_functions.ps1
@@ -13,6 +13,37 @@ function Enable-ShakeToMinimize {
     Write-Host "Shake to Minimize enabled."
 }
 
+function Add-FleetContextMenu {
+    New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT | Out-Null
+    $fleetPath = "C:\Users\mjorgensen\AppData\Local\Programs\Fleet\Fleet.exe"
+
+    # Add for all file types
+    $path = "HKCR:\*\shell\Open with Fleet"
+    New-Item -Path "$path" -Force | Out-Null
+    New-ItemProperty -LiteralPath "$path" -Name "(Default)" -Value "Open with Fleet" -PropertyType String -Force | Out-Null
+    New-ItemProperty -LiteralPath "$path" -Name "Icon" -Value "$fleetPath,0" -PropertyType ExpandString -Force | Out-Null
+    New-Item -Path "$path\command" -Force | Out-Null
+    New-ItemProperty -LiteralPath "$path\command" -Name "(Default)" -Value "$fleetPath `"%1`"" -PropertyType String -Force | Out-Null
+
+    # Add for folders
+    $path = "HKCR:\Directory\shell\Open with Fleet"
+    New-Item -Path "$path" -Force | Out-Null
+    New-ItemProperty -LiteralPath "$path" -Name "(Default)" -Value "Open with Fleet" -PropertyType String -Force | Out-Null
+    New-ItemProperty -LiteralPath "$path" -Name "Icon" -Value "$fleetPath,0" -PropertyType ExpandString -Force | Out-Null
+    New-Item -Path "$path\command" -Force | Out-Null
+    New-ItemProperty -LiteralPath "$path\command" -Name "(Default)" -Value "$fleetPath `"%1`"" -PropertyType ExpandString -Force | Out-Null
+
+    # Add for inside folders
+    $path = "HKCR:\Directory\Background\shell\Open with Fleet"
+    New-Item -Path "$path" -Force | Out-Null
+    New-ItemProperty -LiteralPath "$path" -Name "(Default)" -Value "Open with Fleet" -PropertyType String -Force | Out-Null
+    New-ItemProperty -LiteralPath "$path" -Name "Icon" -Value "$fleetPath,0" -PropertyType ExpandString -Force | Out-Null
+    New-Item -Path "$path\command" -Force | Out-Null
+    New-ItemProperty -LiteralPath "$path\command" -Name "(Default)" -Value "$fleetPath `"%V`"" -PropertyType String -Force | Out-Null
+
+    Remove-PSDrive HKCR
+}
+
 function Add-SublimeTextContextMenu {
     New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT | Out-Null
     $st3Path = "C:\Program Files\Sublime Text 3\sublime_text.exe"
